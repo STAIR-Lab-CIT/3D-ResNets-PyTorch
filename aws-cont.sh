72f4c590bcef16908c519927c24863bfc6145f14
akitakeuchi <takeuchi@stair.center>
2018-09-17 05:47:34 +0000

    aws1-2

--- aws-cont.sh
+++ aws-cont.sh
@@ -20,15 +20,15 @@ EOF
 # this is for yoshikawa datasets (after removing duplicates)
 # batch_size 128 -> unhandled cuda error
 python3 main.py \
-	--learning_rate 1.0e-6 \
+	--learning_rate 1.0e-2 \
 	--n_epochs 250 \
-	--begin_epoch 196 \
-	--resume_path /data4/SA4HDDv20170626/results_resnext101/save_195.pth \
-       --sample_duration 30 \
+	--begin_epoch 86 \
+	--resume_path /data4/SA4HDDv20170626/results_resnext101_224/save_85.pth \
+       --sample_duration 16 \
        --root_path /data4/SA4HDDv20170626 \
        --video_path jpg \
        --annotation_path 3dresnet_STAIRACTIONS.json \
-       --result_path results_resnext101 \
+       --result_path results_resnext101_224 \
        --dataset kinetics --model resnext \
-       --n_classes 100 --sample_size 112 --n_val_samples 3 \
-       --model_depth 101 --batch_size 128 --n_threads 8 --checkpoint 5 --manual_seed 13
+       --n_classes 100 --sample_size 224 --n_val_samples 3 \
+       --model_depth 101 --batch_size 64 --n_threads 8 --checkpoint 5 --manual_seed 13
