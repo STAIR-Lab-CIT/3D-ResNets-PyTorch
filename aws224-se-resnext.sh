0a3b38d7f29fea6c5b928bbec5d640d2e1de51ef
akitakeuchi <takeuchi@stair.center>
2018-11-09 03:55:49 +0000

    volta

---
+++ aws224-se-resnext.sh
@@ -0,0 +1,11 @@
+python3 main.py \
+       --sample_duration 16 \
+       --root_path /data4/SA4HDDv20170626 \
+       --video_path jpg \
+       --annotation_path 3dresnet_STAIRACTIONS.json \
+       --result_path results_se_resnext152_224_dur16_scl7_pj7 \
+       --dataset kinetics --model se_resnext \
+       --initial_scale 1.5 --n_scales 7 --scale_step 0.834 --train_crop 'random' \
+       --projection 7 \
+       --n_classes 100 --sample_size 224 --n_val_samples 3 \
+       --model_depth 152 --batch_size 128 --n_threads 8 --checkpoint 5 --manual_seed 13
